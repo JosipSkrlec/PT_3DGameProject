fcd4ec4bd53fa0cf6436df33c3ae4d8d4963cced
JosipSkrlec <jskrlec1997@gmail.com>
2022-03-20 11:24:30 +0100

    Added concept map design...

---
+++ Assets/Scripts/TESTING_PLASTIC_SCM.cs
@@ -0,0 +1,10 @@
+/*
+Hello Josip and Christian
+
+    This file is used for testing the new collaboration function with PlasticSCM.
+
+    Just add any line below my comment or delete this file to verify if your Unity version already supports PlasticSCM and if
+it is working as intended. Here is the first part of the level design i did.
+
+- Fernando
+*/
--- README.md
+++
@@ -1,1 +0,0 @@
-# PT_3DGameProject
